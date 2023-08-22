be447240542d2a307074252318894f69ad526fd5
MUNICIPALIDAD\mvelez <sistemas@villaallende.gov.ar>
2023-08-22 10:24:37 -0300

    no message

--- WebApiShared/Program.cs
+++ WebApiShared/Program.cs
@@ -29,7 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
-app.UseCors("corsapp");
+//app.UseCors("corsapp");
 app.UseAuthorization();
 app.UseStaticFiles();
 app.MapControllers();
