56ae55810876761624b53389a05d4a4e886ba022
mandrescba <93406941+mandrescba@users.noreply.github.com>
2023-12-13 23:26:18 -0300

    ok

--- WebApiShared/Entities/DALBase.cs
+++ WebApiShared/Entities/DALBase.cs
@@ -1,6 +1,10 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace WebApiShared.Entities
 {
