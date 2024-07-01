ba13859f3df3a9262f94e00a80962baae310f3f9
sistemasvillaallende <107209701+sistemasvillaallende@users.noreply.github.com>
2024-07-01 00:58:53 -0300

    loin cidi

--- WebApiShared/obj/Debug/net6.0/WebApiShared.AssemblyInfo.cs
+++ WebApiShared/obj/Debug/net6.0/WebApiShared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiShared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5096b7a746bee6c51d3935404c12cf684a96408")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8588a261d4e41b18ceea4d94203f154ed9f68747")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiShared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiShared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
